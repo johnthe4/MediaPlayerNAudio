3dfe09cdfae1fdb3f9c711d58d4b052882a004e3
johnthe4 <67112212+johnthe4@users.noreply.github.com>
2021-07-15 00:52:01 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace MediaPlayerNAudio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pauseButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(265, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open Wav File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pauseButton
+            // 
+            this.pauseButton.Enabled = false;
+            this.pauseButton.Location = new System.Drawing.Point(25, 88);
+            this.pauseButton.Name = "pauseButton";
+            this.pauseButton.Size = new System.Drawing.Size(265, 23);
+            this.pauseButton.TabIndex = 1;
+            this.pauseButton.Text = "Play/Pause ";
+            this.pauseButton.UseVisualStyleBackColor = true;
+            this.pauseButton.Click += new System.EventHandler(this.pauseButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(311, 167);
+            this.Controls.Add(this.pauseButton);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Media Player";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button pauseButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Windows.Forms;
+
+
+
+namespace MediaPlayerNAudio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private NAudio.Wave.WaveFileReader wave = null;     //source audio 
+        private NAudio.Wave.DirectSoundOut output = null;   //default direct sound object on machine
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //open file explorer and choose file
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "Wave File (*.wav)|*.wav;";
+            if (open.ShowDialog() != DialogResult.OK) return;
+
+            DisposeWave();
+
+            wave = new NAudio.Wave.WaveFileReader(open.FileName);
+            output = new NAudio.Wave.DirectSoundOut();
+            output.Init(new NAudio.Wave.WaveChannel32(wave));
+
+            output.Play();
+            pauseButton.Enabled = true;
+        }
+
+        private void pauseButton_Click(object sender, EventArgs e)
+        {
+            if (output != null)
+            {
+                if (output.PlaybackState == NAudio.Wave.PlaybackState.Playing) output.Pause();
+                else if (output.PlaybackState == NAudio.Wave.PlaybackState.Paused) output.Play();
+            }
+        }
+
+        private void DisposeWave()
+        {
+            if (output != null)
+            {
+                if (output.PlaybackState == NAudio.Wave.PlaybackState.Playing) output.Stop();
+                output.Dispose();
+                output = null;
+            }
+            if (wave != null)
+            {
+                wave.Dispose();
+                wave = null;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DisposeWave();
+        }
+    }
+}
