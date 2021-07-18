512ca3f856daf1b95f91a9fb254cc8b8a3c518c2
johnthe4 <67112212+johnthe4@users.noreply.github.com>
2021-07-17 22:03:55 -0400

    Update Form1.Designer.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -61,6 +61,7 @@
             this.Controls.Add(this.pauseButton);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Media Player";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.ResumeLayout(false);
